5247600ade6a77916ba1cc80d21557ea6a08bbeb
matej <matej.basta@email.cz>
2022-04-27 16:49:56 +0200

    Category seeder finished

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -14,6 +16,28 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table("categories")->truncate();
+        
+        $categories = [
+            [
+                "name" => "stoly",
+            ],
+            [
+                "name" => "konferenční stolky",
+            ],
+            [
+                "name" => "květináče",
+            ],
+            [
+                "name" => "doplňky",
+            ],
+        ];
+
+        foreach($categories as $category) {
+            Category::create([
+                "name" => $category["name"],
+            ]);
+        }
+
     }
 }
