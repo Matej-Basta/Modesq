905da8a0c9d4729e9d37cbb5843ff11ebc108c67
matej <matej.basta@email.cz>
2022-04-27 17:55:48 +0200

    product seeder in production

--- database/seeders/ColorSeeder.php
+++ database/seeders/ColorSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
+use App\Models\Color;
 
 class ColorSeeder extends Seeder
 {
@@ -14,6 +16,28 @@ class ColorSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table("colors")->truncate();
+
+        $colors = [
+            [
+                "name" => "seda",
+                "picture" => "/img/seda.jpg",
+            ],
+            [
+                "name" => "bila",
+                "picture" => "/img/bila.jpg",
+            ],
+            [
+                "name" => "černá",
+                "picture" => "/img/cerna.jpg",
+            ],
+        ];
+
+        foreach($colors as $color) {
+            Color::create([
+                "name" => $color["name"],
+                "picture" => $color["picture"],
+            ]);
+        };
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
@@ -14,6 +16,14 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table("products")->truncate();
+
+        $products = [
+            [
+                "name" => ,
+                "description" => ,
+                "category_id" => ,
+            ]
+        ]
     }
 }
