dd000d49c1eb19fcef0b51693cad4ce89ac1a204
matej <matej.basta@email.cz>
2022-05-03 11:37:48 +0200

    creating routes in the shop app

--- resources/js/Shop/App.jsx
+++ resources/js/Shop/App.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//custom components
+import Header from "../Components/Header";
 
 export default function App() {
-    return "potapěč";
+    return (
+        <>
+            <Header />
+
+            <BrowserRouter>
+
+                <Routes>
+
+                    <Route path="/obchod/stoly" element={} />
+                    <Route path="/obchod/stolky" element={} />
+
+                </Routes>
+
+            </BrowserRouter>
+        </>
+
+    );
 }
